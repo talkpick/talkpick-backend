44b18cebe58dcfe9550421ac27528d0317899b20
Yejeong, Ham <dev@thelightway.kr>
2025-05-09 10:55:10 +0900

    📦 Chore: TalkPick 백엔드  프로젝트 초기 설정 (#1)

---
+++ src/main/java/com/likelion/backendplus4/talkpick/backend/TalkpickBackendApplication.java
@@ -0,0 +1,13 @@
+package com.likelion.backendplus4.talkpick.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TalkpickBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TalkpickBackendApplication.class, args);
+	}
+
+}
