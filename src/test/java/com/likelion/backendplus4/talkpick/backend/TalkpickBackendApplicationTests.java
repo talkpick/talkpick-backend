7bdcf2070795fa6b1d7c88f6680b21ee5d47d9fe
thelightway <dev@thelightway.kr>
2025-05-09 10:54:43 +0900

    📦 Chore: TalkPick 백엔드  프로젝트 초기 설정

---
+++ src/test/java/com/likelion/backendplus4/talkpick/backend/TalkpickBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.likelion.backendplus4.talkpick.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TalkpickBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
