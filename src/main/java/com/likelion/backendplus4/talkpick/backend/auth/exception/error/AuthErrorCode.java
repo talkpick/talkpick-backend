bcbe7a14c40822b450c24abf811f9b9e6d85099d
pcb7893@naver.com <122460524+chanbyoung@users.noreply.github.com>
2025-05-10 22:13:48 +0900

    ✨ Feat: 커스텀 예외 및 오류 정의

---
+++ src/main/java/com/likelion/backendplus4/talkpick/backend/auth/exception/error/AuthErrorCode.java
@@ -0,0 +1,30 @@
+package com.likelion.backendplus4.talkpick.backend.auth.exception.error;
+
+import com.likelion.backendplus4.talkpick.backend.common.exception.error.ErrorCode;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+
+@RequiredArgsConstructor
+public enum AuthErrorCode implements ErrorCode {
+
+    AUTHENTICATION_FAILED(HttpStatus.UNAUTHORIZED, 220001, "인증에 실패하였습니다.");
+
+    private final HttpStatus status;
+    private final int code;
+    private final String message;
+    
+    @Override
+    public HttpStatus httpStatus() {
+        return status;
+    }
+
+    @Override
+    public int codeNumber() {
+        return code;
+    }
+
+    @Override
+    public String message() {
+        return message;
+    }
+}
