f5dcfd4b5ba9f12c487f5427e9c906db1a7fc577
pcb7893@naver.com <122460524+chanbyoung@users.noreply.github.com>
2025-05-13 17:43:58 +0900

    ✨ Feat: 인증 test api 추가

---
+++ src/main/java/com/likelion/backendplus4/talkpick/backend/user/presentation/controller/UserController.java
@@ -0,0 +1,23 @@
+package com.likelion.backendplus4.talkpick.backend.user.presentation.controller;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.likelion.backendplus4.talkpick.backend.common.annotation.security.LoginUser;
+import com.likelion.backendplus4.talkpick.backend.common.response.ApiResponse;
+
+@RestController
+@RequestMapping("/api/user")
+public class UserController {
+
+	/**
+	 * 재발급 테스트 코드입니다.
+	 * TODO 삭제
+	 */
+	@GetMapping("/test")
+	public ResponseEntity<ApiResponse<Long>> test(@LoginUser Long userId) {
+		return ApiResponse.success(userId);
+	}
+}
