e0f643bafcfc4a5ba01ac0e891203b548d3b7acb
HaechangLee <112938092+HaechangLee@users.noreply.github.com>
2025-05-19 09:44:07 +0900

    ✨  Feat: websocketConfig 유저정보 받아오는 로직 추가(작업중)

--- src/main/java/com/likelion/backendplus4/talkpick/backend/chat/config/WebSocketConfig.java
+++ src/main/java/com/likelion/backendplus4/talkpick/backend/chat/config/WebSocketConfig.java
@@ -1,11 +1,24 @@
 package com.likelion.backendplus4.talkpick.backend.chat.config;
 
+
+import com.likelion.backendplus4.talkpick.backend.auth.application.port.out.SecurityPort;
+import com.likelion.backendplus4.talkpick.backend.auth.infrastructure.security.JwtAuthentication;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.messaging.Message;
+import org.springframework.messaging.MessageChannel;
+import org.springframework.messaging.simp.config.ChannelRegistration;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
+import org.springframework.messaging.simp.stomp.StompCommand;
+import org.springframework.messaging.simp.stomp.StompHeaderAccessor;
+import org.springframework.messaging.support.ChannelInterceptor;
+import org.springframework.messaging.support.MessageHeaderAccessor;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
 import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
 import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
+import org.springframework.web.socket.server.support.HttpSessionHandshakeInterceptor;
 
 /**
  * WebSocket 및 STOMP 기반 메시징을 설정하는 Configuration 클래스.
@@ -21,6 +34,7 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
     private final int relayPort;
     private final String username;
     private final String password;
+    private final JwtAuthentication jwtAuthentication;
 
     /**
      * 생성자 주입을 통해 프로퍼티 값을 받습니다.
@@ -35,11 +49,13 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
             @Value("${spring.rabbitmq.host}") String relayHost,
             @Value("${spring.rabbitmq.stomp-port}") int relayPort,
             @Value("${spring.rabbitmq.username}") String username,
-            @Value("${spring.rabbitmq.password}") String password) {
+            @Value("${spring.rabbitmq.password}") String password,
+            JwtAuthentication jwtAuthentication) {
         this.relayHost = relayHost;
         this.relayPort = relayPort;
         this.username = username;
         this.password = password;
+        this.jwtAuthentication = jwtAuthentication;
     }
     /**
      * 클라이언트가 연결할 STOMP 엔드포인트 등록
@@ -51,6 +67,7 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
         registry.addEndpoint("/ws-chat")
+                .addInterceptors(new HttpSessionHandshakeInterceptor())
 //                .setAllowedOriginPatterns("*") // TODO: nginx 설정 시 제거
                 .withSockJS();
     }
@@ -77,5 +94,30 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
 
         // 클라이언트 -> 서버 메시지 prefix
         config.setApplicationDestinationPrefixes("/app");
+    }
+
+    @Override
+    public void configureClientInboundChannel(ChannelRegistration registration) {
+        registration.interceptors(new ChannelInterceptor() {
+            @Override
+            public Message<?> preSend(Message<?> message, MessageChannel channel) {
+                StompHeaderAccessor accessor =
+                        MessageHeaderAccessor.getAccessor(message, StompHeaderAccessor.class);
+
+                if (StompCommand.CONNECT.equals(accessor.getCommand())) {
+                    String authHeader = accessor.getFirstNativeHeader("Authorization");
+                    if (authHeader != null && authHeader.startsWith("Bearer ")) {
+                        System.out.println("Received authentication header: " + authHeader);
+                        String token = authHeader.substring(7);
+                        Authentication auth = jwtAuthentication.validateAndGetAuthentication(token);
+                        accessor.setUser(auth);
+                        System.out.println("User authenticated: " + auth.getName());
+                        System.out.println(auth.getPrincipal());
+                        System.out.println(auth.getAuthorities());
+                    }
+                }
+                return message;
+            }
+        });
     }
 }
