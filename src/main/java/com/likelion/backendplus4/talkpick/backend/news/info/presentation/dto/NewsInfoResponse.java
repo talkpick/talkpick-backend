3b5bb196a3d776b3ea8d84fa0b76cdbdc1041b59
thelightway <dev@thelightway.kr>
2025-05-19 15:39:33 +0900

    ✨ Feature: 최근 뉴스 조회 추가

---
+++ src/main/java/com/likelion/backendplus4/talkpick/backend/news/info/presentation/dto/NewsInfoResponse.java
@@ -0,0 +1,4 @@
+package com.likelion.backendplus4.talkpick.backend.news.info.presentation.dto;
+
+public class NewsInfoResponse {
+}
---
+++ src/main/java/com/likelion/backendplus4/talkpick/backend/news/info/presentation/mapper/NewsInfoResponseMapper.java
@@ -0,0 +1,4 @@
+package com.likelion.backendplus4.talkpick.backend.news.info.presentation.mapper;
+
+public class NewsInfoResponseMapper {
+}
