257aab6fee32b643294631b28acbf7b772c98df4
JUNG ANSIK <jaszzzzz@naver.com>
2025-05-20 10:53:35 +0900

    ✨  Feature: #66 유사 뉴스 검색 기능 (#88)
    * ✨  Feature/#7-Elasticsearch-검색-기능
    
    * ♻️ Refactor: NewsSearchRequest객체가 이제 제대로 기본값을 반영합니다.
    
    * ♻️ Refactor: NewsSearchController의 가독성을 위해 static import 사용구조로 변경하였습니다., 유사 뉴스 검색 기능을 위해 필요한 메서드 및 객체를 추가하였습니다.
    
    * ✨  Feat: 유사 뉴스 검색 기능 구현

--- src/main/java/com/likelion/backendplus4/talkpick/backend/search/application/port/in/mapper/NewsSearchRequestMapper.java
+++ src/main/java/com/likelion/backendplus4/talkpick/backend/search/application/port/in/mapper/NewsSearchRequestMapper.java
@@ -3,7 +3,9 @@ package com.likelion.backendplus4.talkpick.backend.search.application.port.in.ma
 import org.springframework.stereotype.Component;
 
 import com.likelion.backendplus4.talkpick.backend.search.domain.model.NewsSearch;
+import com.likelion.backendplus4.talkpick.backend.search.domain.model.NewsSimilarSearch;
 import com.likelion.backendplus4.talkpick.backend.search.presentation.controller.dto.request.NewsSearchRequest;
+import com.likelion.backendplus4.talkpick.backend.search.presentation.controller.dto.request.NewsSimilarSearchRequest;
 
 /**
  * 요청 DTO를 도메인 모델로 변환하는 매퍼 클래스
@@ -21,6 +23,10 @@ public class NewsSearchRequestMapper {
 	 * @since 2025-05-15
 	 */
 	public static NewsSearch toDomain(NewsSearchRequest req) {
-		return new NewsSearch(req.getQ(), req.getPage(), req.getSize());
+		return new NewsSearch(req.q(), req.page(), req.size());
+	}
+
+	public static NewsSimilarSearch toDomain(NewsSimilarSearchRequest req) {
+		return new NewsSimilarSearch(req.newsId(), req.page(), req.size());
 	}
 }
