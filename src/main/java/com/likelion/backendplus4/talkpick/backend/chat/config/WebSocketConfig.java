7f87e0432577fdb2bd104030398f25e95e598426
Yejeong, Ham <dev@thelightway.kr>
2025-05-21 12:44:28 +0900

    🐛 Fix: 웹 소켓 오류 수정 (#98)

--- src/main/java/com/likelion/backendplus4/talkpick/backend/chat/config/WebSocketConfig.java
+++ src/main/java/com/likelion/backendplus4/talkpick/backend/chat/config/WebSocketConfig.java
@@ -56,7 +56,8 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
         registry.addEndpoint("/ws-chat")
-                .withSockJS();
+            .setAllowedOrigins("https://talkpick.techlog.dev")
+            .withSockJS();
     }
 
     /**
