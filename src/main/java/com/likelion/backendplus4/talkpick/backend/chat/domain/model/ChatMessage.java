7765277bc2ed13697bd728c6b27ef84df865fd3d
pcb7893@naver.com <122460524+chanbyoung@users.noreply.github.com>
2025-05-27 23:53:59 +0900

    ♻️ Refactor: 주석처리한 메서드 삭제

--- src/main/java/com/likelion/backendplus4/talkpick/backend/chat/domain/model/ChatMessage.java
+++ src/main/java/com/likelion/backendplus4/talkpick/backend/chat/domain/model/ChatMessage.java
@@ -2,8 +2,6 @@ package com.likelion.backendplus4.talkpick.backend.chat.domain.model;
 
 import java.time.LocalDateTime;
 
-import com.likelion.backendplus4.talkpick.backend.chat.exception.ChatException;
-import com.likelion.backendplus4.talkpick.backend.chat.exception.error.ChatErrorCode;
 
 import lombok.Builder;
 import lombok.Getter;
@@ -18,18 +16,4 @@ public class ChatMessage {
     private final LocalDateTime timestamp;
     private final MessageType messageType;
 
-    // public ChatMessage(Long chatId, String articleId, String sender, String content,LocalDateTime timestamp, MessageType messageType) {
-    //     if(articleId == null || articleId.isEmpty()) {
-    //         throw new ChatException(ChatErrorCode.INVALID_ARTICLE_ID);
-    //     }
-    //     if(sender == null || sender.isEmpty()) {
-    //         throw new ChatException(ChatErrorCode.INVALID_SENDER);
-    //     }
-    //     this.chatId = chatId;
-    //     this.articleId = articleId;
-    //     this.sender = sender;
-    //     this.content = content;
-    //     this.timestamp = timestamp;
-    //     this.messageType = messageType;
-    // }
 }
