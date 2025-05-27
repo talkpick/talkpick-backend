b7beb6ede887a689e35492ba1b264e6414e05678
pcb7893@naver.com <122460524+chanbyoung@users.noreply.github.com>
2025-05-27 23:24:54 +0900

    ♻️ Refactor: 스케쥴링 로직 삭제

--- src/main/java/com/likelion/backendplus4/talkpick/backend/TalkpickBackendApplication.java
+++ src/main/java/com/likelion/backendplus4/talkpick/backend/TalkpickBackendApplication.java
@@ -2,10 +2,8 @@ package com.likelion.backendplus4.talkpick.backend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
-@EnableScheduling
 public class TalkpickBackendApplication {
 
 	public static void main(String[] args) {
